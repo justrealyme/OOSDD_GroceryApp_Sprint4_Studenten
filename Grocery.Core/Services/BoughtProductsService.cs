18314d3fd3dd333f6e141d13495d3484b94562c4
justrealyme <rickertreimert@gmail.com>
2025-10-03 16:20:09 +0200

    uc13
    uc13

--- Grocery.Core/Services/BoughtProductsService.cs
+++ Grocery.Core/Services/BoughtProductsService.cs
@@ -1,5 +1,4 @@
-﻿
-using Grocery.Core.Interfaces.Repositories;
+﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
 
@@ -11,16 +10,40 @@ namespace Grocery.Core.Services
         private readonly IClientRepository _clientRepository;
         private readonly IProductRepository _productRepository;
         private readonly IGroceryListRepository _groceryListRepository;
+
         public BoughtProductsService(IGroceryListItemsRepository groceryListItemsRepository, IGroceryListRepository groceryListRepository, IClientRepository clientRepository, IProductRepository productRepository)
         {
-            _groceryListItemsRepository=groceryListItemsRepository;
-            _groceryListRepository=groceryListRepository;
-            _clientRepository=clientRepository;
-            _productRepository=productRepository;
+            _groceryListItemsRepository = groceryListItemsRepository;
+            _groceryListRepository = groceryListRepository;
+            _clientRepository = clientRepository;
+            _productRepository = productRepository;
         }
+
         public List<BoughtProducts> Get(int? productId)
         {
-            throw new NotImplementedException();
+            if (productId == null) return new List<BoughtProducts>();
+
+            List<BoughtProducts> result = new();
+
+            var groceryListItems = _groceryListItemsRepository.GetAll()
+                .Where(item => item.ProductId == productId.Value)
+                .ToList();
+
+            foreach (var item in groceryListItems)
+            {
+                var groceryList = _groceryListRepository.Get(item.GroceryListId);
+                if (groceryList == null) continue;
+
+                var client = _clientRepository.Get(groceryList.ClientId);
+                if (client == null) continue;
+
+                var product = _productRepository.Get(item.ProductId);
+                if (product == null) continue;
+
+                result.Add(new BoughtProducts(client, groceryList, product));
+            }
+
+            return result;
         }
     }
 }
